fc05be891bb76a739a59592465dbd7756525da5e
jeomseon0516 <126733377+jeomseon0516@users.noreply.github.com>
2026-07-29 11:47:40 +0900

    feat: own localized string inspector tooling

--- README.md
+++ README.md
@@ -1,15 +1,21 @@
 # Jeomseon Unity Localization
 
-Extensions and components for Unity Localization.
+Unity Localization을 위한 런타임 확장, `LocalizedStringAttribute` 및 전용 PropertyDrawer를 제공합니다.
 
 ## 설치
 
 OpenUPM 등록 전에는 Package Manager의 **Add package from git URL**에서 다음 주소를 사용합니다.
 
 ```text
-https://github.com/jeomseon0516/Unity.Localization.git#v0.1.0
+https://github.com/jeomseon0516/Unity.Localization.git#v0.2.0
 ```
 
 ## 리팩토링 방침
 
 Unity가 제공하는 동등 기능과 비교해 대체 가능한 코드는 소스의 한글 TODO 주석과 CHANGELOG의 Unreleased 항목에서 추적합니다.
+
+## 포함 기능
+
+- `LocalizedString` 런타임 확장
+- `Jeomseon.Localization.LocalizedStringAttribute`
+- String Table과 Entry를 편집하는 전용 PropertyDrawer
---
+++ Runtime/Attributes/LocalizedStringAttribute.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Localization;
+
+namespace Jeomseon.Localization
+{
+    [AttributeUsage(AttributeTargets.Field), Conditional("UNITY_EDITOR")]
+    public sealed class LocalizedStringAttribute : PropertyAttribute
+    {
+        public bool CanSelectTable { get; }
+        public string TableName { get; }
+        public string EntryKey { get; }
+
+        public LocalizedStringAttribute(bool canSelectTable = false, string tableName = null, string entryKey = null)
+        {
+            CanSelectTable = canSelectTable;
+            TableName = tableName;
+            EntryKey = entryKey;
+        }
+
+    }
+}
